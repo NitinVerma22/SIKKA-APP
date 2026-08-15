2c9e8e13bdb45bd3e16b9ed88b80d0e2723322c5
eliteassignment-commits <eliteassignment@gmail.com>
2026-08-15 18:49:39 +0530

    bump: version 1.0.8+9 (AppConfig & pubspec.yaml sync)

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   // Current installed version of the app
-  static const String currentVersion = '1.0.7';
+  static const String currentVersion = '1.0.8';
 
   // Agora App ID for Voice/Video calling (Create a free project on console.agora.io to get one)
   static const String agoraAppId = '8ab6624ff4784a618fe9e3366ad5c40f'; 
