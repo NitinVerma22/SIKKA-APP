54be97a1ad33665b235e78d355fd1cea171a71d5
eliteassignment-commits <eliteassignment@gmail.com>
2026-08-26 14:30:26 +0530

    Fix localNotifications.show named arguments

--- lib/main.dart
+++ lib/main.dart
@@ -207,10 +207,10 @@ Future<void> _showLocalNotification(RemoteMessage message) async {
   });
 
   await localNotifications.show(
-    message.hashCode,
-    title,
-    body,
-    details,
+    id: message.hashCode,
+    title: title,
+    body: body,
+    notificationDetails: details,
     payload: payloadData,
   );
 }
